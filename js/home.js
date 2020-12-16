20a86c62e12e26a339e7c3ef881f243665d1d1f6
wiwi1806 <lucile.gombert.lg@gmail.com>
2020-12-16 09:40:24 +0100

    commit home page

--- js/home.js
+++ js/home.js
@@ -1,47 +1,55 @@
 const products = document.getElementById("products");
+
+// Appel de l'API
 fetch("http://localhost:3000/api/cameras")
+    // Permet de récupérer une réponse au format json
     .then(function (response) {
         return response.json();
-
     })
+    // Permet d'accéder aux produits
     .then(function(items) {
-        displayItems(items)
-        
+        displayItems(items);
     });
 
+// Affiche le contenu de chaque carte produit dans le code HTMl
 function displayItems(products) {
-        products.forEach(product => {
-                console.log(product);
-    
-                const productCard = document.createElement('div');
-                productCard.setAttribute('id', product._id);
-                productCard.setAttribute('class', 'card p-3 m-3');
-    
-                const productName = document.createElement('h3');
-                productName.setAttribute('class', 'name');
-                productName.innerHTML = product.name;
-                productCard.appendChild(productName);
-    
-                const productImg = document.createElement('img');
-                productImg.setAttribute('src', product.imageUrl);
-                productImg.setAttribute('style', 'width: 500px;');
-                productCard.appendChild(productImg);
-    
-                const productDescription = document.createElement('p');
-                productDescription.setAttribute('class', 'description');
-                productDescription.innerHTML = product.description;
-                productCard.appendChild(productDescription);
-    
-                const productLenses = document.createElement('p');
-                productLenses.setAttribute('class', 'lenses');
-                productLenses.innerHTML = product.lenses;
-                productCard.appendChild(productLenses);
-    
-                const productPrice = document.createElement('p');
-                productPrice.setAttribute('class', 'price');
-                productPrice.innerHTML = product.price.toFixed(2) + ' €';
-                productCard.appendChild(productPrice);          
-    
-                document.getElementById('products').appendChild(productCard);
-        });
+    products.forEach(product => { // remplace le i de for (let i = 0; i < products.length; i++)
+        console.log(product);
+        
+        const productCard = document.createElement('div');
+        productCard.setAttribute('id', product._id);
+        productCard.setAttribute('class', 'card p-3 m-3');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.imageUrl);
+        productImg.setAttribute('class', 'img-fluid');
+        productImg.setAttribute('style', 'max-width: 400px');
+        productCard.appendChild(productImg);
+
+        const productName = document.createElement('h3');
+        productName.setAttribute('class', 'name card-title pt-2');
+        productName.innerHTML = product.name;
+        productCard.appendChild(productName);
+
+        const productPrice = document.createElement('p');
+        productPrice.setAttribute('class', 'price');
+        productPrice.innerHTML = product.price.toFixed(2) /100 + ' €';
+        productCard.appendChild(productPrice);          
+
+        const productLink = document.createElement('a');
+        productLink.setAttribute('href', 'product.html?id=' + product._id);
+        productLink.setAttribute('class', 'link btn bg-pink btn-outline-dark');
+        productLink.innerHTML = 'Voir plus ➔ ';
+        productCard.appendChild(productLink);   
+
+        document.getElementById('products').appendChild(productCard);
+    });
 };
+
+const link = document.querySelector('.btn');
+
+link.addEventListener('click', fenetre);
+
+function fenetre() {
+    window.open('http://product.html', '_blank');
+}
