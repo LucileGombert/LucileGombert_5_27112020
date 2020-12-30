acc19ad9ca1da8276d4dccff1c2f1f6ae7e4ba8a
wiwi1806 <lucile.gombert.lg@gmail.com>
2020-12-30 15:53:52 +0100

    plus/minus quantity & remove product cart

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,10 @@
-// Initialise la constante - Positionne dans la div nommée "cartContainer"
-// let basket = document.getElementById('cartContainer');
-let affichagePanier = document.getElementById('cartContainer');
-
+// Initialise les variables utilisées dans la page "product.js"
 let basket =  JSON.parse(localStorage.getItem('basket'));
 let totalCost = JSON.parse(localStorage.getItem('totalCost'));
+let basketCost = JSON.parse(localStorage.getItem('totalCost'));
 
+// Initialise la variable - Positionne dans la div nommée "cartContainer"
+let cartContainer = document.getElementById('cartContainer');
 
 // Si le panier est vide, affiche 'Votre panier est vide !', sinon affiche 'Votre panier'
 if (basket == null || basket.length == 0) {
@@ -25,44 +25,46 @@ if (basket == null || basket.length == 0) {
 // Si le panier contient des produits, il les affiche
 if(basket.length > 0) { 
     for(let product of basket) {
-                 
+        
+        // Crée la div qui contient chaque produit
         const basketItem = document.createElement('div');
         basketItem.setAttribute('class', 'row p-3 align-items-center border-bottom');
         cartContainer.appendChild(basketItem);
 
+        // Affiche l'image du produit 
         const basketProductImage = document.createElement('img');
         basketProductImage.setAttribute('src', product.image);
         basketProductImage.setAttribute('class', 'img-fluid col-lg-1 col-xl-2');
         basketProductImage.setAttribute('style', 'max-width: 200px');
         basketItem.appendChild(basketProductImage);
-
+        
+        // Affiche le nom du produit 
         const basketProductName = document.createElement('p');
         basketProductName.setAttribute('class', 'col-lg-2');
         basketProductName.innerHTML = product.name;
         basketItem.appendChild(basketProductName);
 
+        // Affiche le prix du produit 
         const basketProductPrice = document.createElement('p');
         basketProductPrice.setAttribute('class', 'col-lg-2  text-center');
         basketProductPrice.innerHTML =  product.price /100 + ',00 €';
         basketItem.appendChild(basketProductPrice);
 
+        // Affiche la quantité 
         const basketProductQuantity = document.createElement('div');
         basketProductQuantity.setAttribute('class', 'quantity col-md-2 col-lg-3 text-center');
-        // basketProductQuantity.innerHTML = product.quantity;  
-        basketProductQuantity.innerHTML += (`<button class="minus disabled btn bg-pink btn-outline-dark">-</button>
-                                            <input id="quantity" class=" border p-2 col-3" type="text" value="${product.quantity}"</input>
-                                            <button class="plus btn bg-pink btn-outline-dark">+</button>`)
+        basketProductQuantity.innerHTML += (`<button class="minus disabled btn bg-pink btn-outline-dark" data-item="${product.id}">-</button>
+                                            <input class=" border p-2 col-3" type="text" value="${product.quantity}"</input>
+                                            <button class="plus btn bg-pink btn-outline-dark" data-item="${product.id}">+</button>`)
         basketItem.appendChild(basketProductQuantity);
 
-        let totalPrice = document.getElementsByClassName('totalPrice');
-        totalPrice = product.price/100 * product.quantity;
+        // Affiche le prix total par produit 
+        const basketProductTotalPrice = document.createElement('p');
+        basketProductTotalPrice.setAttribute('class', 'totalPrice col-lg-2 text-center');
+        basketProductTotalPrice.innerHTML = product.price/100 * product.quantity + ',00 €';
+        basketItem.appendChild(basketProductTotalPrice);
 
-        const basketProducttotalPrice = document.createElement('p');
-        basketProducttotalPrice.setAttribute('class', 'totalPrice col-lg-2 text-center');
-        basketProducttotalPrice.innerHTML = totalPrice + ',00 €';
-        basketItem.appendChild(basketProducttotalPrice);
-
-        // Bouton de suppression d'un produit du panier
+        // Affiche le bouton poubelle pour la suppression d'un produit
         const divRemoveButton = document.createElement('div');
         basketItem.appendChild(divRemoveButton);
 
@@ -75,71 +77,98 @@ if(basket.length > 0) {
         </svg>`);
         divRemoveButton.appendChild(removeButton);
 
-        
-        
-        // removeButton.addEventListener('click', function() {
-        //     let id = this.getAttribute("data-item");
-        //     console.log("id poubelle", id);
-        // });   
-
+        // Affiche le prix total du panier 
         const totalBasketPrice = document.getElementById('totalBasketPrice');
         totalBasketPrice.innerHTML = totalCost /100 + ',00 €';
 
-        
+
+        // Modifie la quantité d'un produit (-)
+        let minusButton = document.querySelector('.minus');
+        minusButton.addEventListener('click', function() {
+            let id = this.getAttribute("data-item");
+
+            // Soustrait 1 à la quantité d'un produit si l'attribut id de la poubelle est identique à celui du produit
+            for (let i=0; i !=basket.length; i++){
+                if (basket[i].id === id){
+                    product.quantity--;
+                    break;
+                }
+            }
+            // Met à jour la quantité dans le local storage
+            localStorage.setItem("basket", JSON.stringify(basket));
+            // Met à jour le prix total du panier
+            localStorage.setItem('totalCost', basketCost - product.price);
+            // Recharge la page
+            window.location.reload();
+        });
+        // Empêche de cliquer sur - lorsque la quantité du produit est de 1
+        if(product.quantity == 1) {
+            document.querySelector('.minus').setAttribute('disabled','disabled');
+        }
+
+
+        // Modifie la quantité d'un produit (+)
+        let plusButton = document.querySelector('.plus');
+        plusButton.addEventListener('click', function() {
+            let id = this.getAttribute("data-item");
+
+            // Ajoute 1 à la quantité d'un produit si l'attribut "id" de la poubelle est identique à celui du produit
+            for (let i=0; i !=basket.length; i++){
+                if (basket[i].id === id){
+                    product.quantity++;
+                    break;
+                }
+            }
+            // Met à jour la quantité dans le local storage
+            localStorage.setItem("basket", JSON.stringify(basket));
+            // Met à jour le prix total du panier
+            localStorage.setItem('totalCost', basketCost + product.price);
+            // Recharge la page
+            window.location.reload();
+        }); 
+        // Supprime l'attribut et la classe "disabled" lorsque la quantité du produit est supérieure à 1
+        if(product.quantity > 1) {
+            document.querySelector('.minus').removeAttribute('disabled');
+            document.querySelector('.minus').classList.remove('disabled');
+        }
+
+
+        // Supprime un produit du panier
+        let removeButtons = document.querySelector('.removeButton');
+        removeButtons.addEventListener('click', function() {
+            let id = this.getAttribute("data-item");
+            
+            // Supprime le produit du panier si l'attribut "id" de la poubelle est identique à celui du produit
+            for (let i=0; i !=basket.length; i++){
+                if (basket[i].id === id){
+                    basket.splice(i,1);
+                    break;
+                }
+            }
+            // Met à jour le panier dans le local storage
+            localStorage.setItem("basket", JSON.stringify(basket));
+            // Met à jour le prix total du panier
+            localStorage.setItem('totalCost', basketCost - (product.price*product.quantity));
+            // Recharge la page
+            window.location.reload();
+        });
     }
 }
 
-// Modifie la quantité de produit 
-let valueCount;
+ 
 
-document.querySelector('.plus').addEventListener('click', function() {
-    valueCount = document.getElementById('quantity').value;
-    valueCount++;
-    document.getElementById('quantity').value = valueCount;
-
-    if(valueCount > 1) {
-        document.querySelector('.minus').removeAttribute('disabled');
-        document.querySelector('.minus').classList.remove('disabled');
-    }
-    localStorage.setItem("basket", JSON.stringify(basket));
-})
-
-document.querySelector('.minus').addEventListener('click', function() {
-    valueCount = document.getElementById('quantity').value;
-    valueCount--;
-    document.getElementById('quantity').value = valueCount;
-
-    if(valueCount == 1) {
-        document.querySelector('.minus').setAttribute('disabled','disabled');
-    }
-})
-
-// Supprime un produit du panier
-let removeButton = document.querySelector('.removeButton');
-removeButton.addEventListener('click', function() {
-    let id = this.getAttribute("data-item");
-    console.log("id poubelle", id);
-
-    for (let i=0; i !=basket.length; i++){
-        if (basket[i].id === id){
-            basket.splice(i,1);
-            break;
-        }
-    }
-    localStorage.setItem("basket", JSON.stringify(basket));
-    window.location.href = "cart.html";
-}); 
 
 // Formulaire de commande
 const submitForm = document.getElementById('orderButton');
 
-submitForm.addEventListener('click',formValidation);
-
-function formValidation(e) {
+// Valide les champs du formulaire au clic sur le bouton "commander"
+submitForm.addEventListener('click',function(e) {
     const lastName = document.getElementById('lastName');
     const lastNameMissing = document.getElementById('lastNameMissing');
+    // Vérifie que le nom comporte seulement les caractères attendus
     const lastNameValidation = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?/i;
 
+    // Si le champ "Nom" est vide ou s'il ne respecte pas la regex, un message d'erreur s'affiche et bloque l'envoi du formulaire
     if (lastName.validity.valueMissing) {
         e.preventDefault();
         lastNameMissing.textContent = 'Ce champ est obligatoire';
@@ -154,8 +183,10 @@ function formValidation(e) {
 
     const firstName = document.getElementById('firstName');
     const firstNameMissing = document.getElementById('firstNameMissing');
+    // Vérifie que le prénom comporte seulement les caractères attendus
     const firstNameValidation = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?/i;
-
+    
+    // Si le champ "Prénom" est vide ou s'il ne respecte pas la regex, un message d'erreur s'affiche et bloque l'envoi du formulaire
     if (firstName.validity.valueMissing) {
         e.preventDefault();
         firstNameMissing.textContent = 'Ce champ est obligatoire';
@@ -170,8 +201,10 @@ function formValidation(e) {
 
     const mail = document.getElementById('mail');
     const mailMissing = document.getElementById('mailMissing');
+    // Vérifie que le mail comporte seulement les caractères attendus
     const mailValidation = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}/;
 
+    // Si le champ "Mail" est vide ou s'il ne respecte pas la regex, un message d'erreur s'affiche et bloque l'envoi du formulaire
     if (mail.validity.valueMissing) {
         e.preventDefault();
         mailMissing.textContent = 'Ce champ est obligatoire';
@@ -186,8 +219,10 @@ function formValidation(e) {
 
     const address = document.getElementById('address');
     const addressMissing = document.getElementById('addressMissing');
+    // Vérifie que l'adresse comporte seulement les caractères attendus
     const adressValidation = /^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)*/i;
 
+    // Si le champ "Adresse" est vide ou s'il ne respecte pas la regex, un message d'erreur s'affiche et bloque l'envoi du formulaire
     if (address.validity.valueMissing) {
         e.preventDefault();
         addressMissing.textContent = 'Ce champ est obligatoire';
@@ -202,8 +237,10 @@ function formValidation(e) {
 
     const zip = document.getElementById('zip');
     const zipMissing = document.getElementById('zipMissing');
+    // Vérifie que le code postal comporte seulement les caractères attendus
     const zipValidation = /^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B))[0-9]{3}$/;
     
+    // Si le champ "Code postal" est vide ou s'il ne respecte pas la regex, un message d'erreur s'affiche et bloque l'envoi du formulaire
     if (zip.validity.valueMissing) {
         e.preventDefault();
         zipMissing.textContent = 'Ce champ est obligatoire';
@@ -218,8 +255,10 @@ function formValidation(e) {
 
     const city = document.getElementById('city');
     const cityMissing = document.getElementById('cityMissing');
+    // Vérifie que la ville comporte seulement les caractères attendus
     const cityValidation = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?/i;
 
+    // Si le champ "Ville" est vide ou s'il ne respecte pas la regex, un message d'erreur s'affiche et bloque l'envoi du formulaire
     if (city.validity.valueMissing) {
         e.preventDefault();
         cityMissing.textContent = 'Ce champ est obligatoire';
@@ -231,7 +270,7 @@ function formValidation(e) {
     } else {
         cityMissing.textContent = '';
     }  
-}
+});
 
 // Validation de la commande
 let orderValidation = document.getElementById("formContent");
@@ -246,7 +285,7 @@ orderValidation.addEventListener("submit", function (e){
     let cityForm = document.getElementById("city").value;
     let mailForm = document.getElementById("mail").value;
 
-    // Tableau de produits envoyé au serveur contenant l'id des produits à commandés
+    // Tableau de produits envoyé au serveur contenant l'id des produits commandés
     let products = [];
     for (let i=0; i < basket.length; i++) {
         products.push(basket[i].id)
@@ -263,20 +302,25 @@ orderValidation.addEventListener("submit", function (e){
         },
         products: products,
     }
-         
+    
+    // Envoi la requête post au serveur
     fetch("http://localhost:3000/api/cameras/order", {
         method: 'POST',
         headers:{
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(orderContent),
-    })
+        })
+
+        // Récupère une réponse au format json
         .then(function(response) {
             return response.json();
         })
+
+        // Affiche le résultat sur la page "orderConfirmation.html"
         .then(function(result) {
             window.location.href = `orderConfirmation.html?id=${result.orderId}&price=${totalCost /100 + ',00 €'}`
         });
-        
+    // Vide le local storage
     localStorage.clear() 
-})
+});
